a28cea75ab141bf606c334a0b551dfb99f9e6dc5
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-09-13 05:55:19 +0700

    refactor: change file name

---
+++ Viginere/viginere-encrypt.py
@@ -0,0 +1,88 @@
+alphabet = [
+    'A', 
+    'B', 
+    'C', 
+    'D', 
+    'E', 
+    'F', 
+    'G', 
+    'H', 
+    'I', 
+    'J', 
+    'K', 
+    'L', 
+    'M', 
+    'N', 
+    'O', 
+    'P', 
+    'Q', 
+    'R', 
+    'S', 
+    'T', 
+    'U', 
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+]
+
+text = ""
+with open('plain_text.txt', 'r') as file:
+    for sentence in file:
+        text += sentence
+
+kata_pembangkit = input("Masukan Kata Pembangkit: ")
+
+plain_text = text.upper().replace(" ", "").replace("\n", "")
+
+clean_plain_text = ""
+
+for char in plain_text:
+    if char in alphabet:
+        clean_plain_text += char
+
+kata_pembangkit = kata_pembangkit.upper().replace(" ", "")
+
+kunci = ""
+
+for index in range(len(clean_plain_text)):
+    kunci += kata_pembangkit[index%len(kata_pembangkit)]
+
+cipher = []
+
+for i in range(len(clean_plain_text)):
+    plain_text_index = alphabet.index(clean_plain_text[i])
+    kunci_index = alphabet.index(kunci[i])
+    
+    result_index = (plain_text_index + kunci_index)%26
+    
+    cipher.append(alphabet[result_index])
+    
+    # print("Plain Char = {}, index = {}".format(clean_plain_text[i], plain_text_index))
+    # print("Kunci Char = {}, index = {}".format(kunci[i], kunci_index))
+    # print("Formula = ({} + {})mod 26 = {} ".format(plain_text_index, kunci_index, result_index))
+    # print("Cipher Char = {}".format(alphabet[result_index]))
+    # print()
+
+cipher_text = ""
+for char in cipher:
+    cipher_text += char
+
+print()
+print()
+print("Text :")
+print(text)
+print("Cleaned Text (Plain Text) : ")
+print(clean_plain_text)
+print()
+print("Key : ")
+print(kunci)
+print()
+print("Cipher Text : ")
+print(cipher_text)
+
+with open('cipher_text.txt', 'w') as file:
+    file.write(cipher_text)
+
+print("Success Generate Cipher Text File!")
